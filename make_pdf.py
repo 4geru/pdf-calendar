a706cdab3fac47873f9b3b4d00232d0da39f46df
Rp7rf <westhouse51@gmail.com>
2017-11-19 19:09:33 +0900

    calendarの作成

--- make_pdf.py
+++ make_pdf.py
@@ -4,61 +4,115 @@ from reportlab.pdfgen import canvas
 from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.cidfonts import UnicodeCIDFont
 from reportlab.lib.pagesizes import letter, landscape
+from datetime import date, timedelta
 import reportlab.lib.colors as color
 from PIL import Image
+import sys
 
 class Point:
-    def __init__(self, width, height):
-        self.width  = width
-        self.height = height
+  def __init__(self, width, height):
+    self.width  = width
+    self.height = height
 
 class Config:
-    def __init__(self):
-        self.height = 792
-        self.width  = 612
-        self.title  = Point(0,40)
-        self.image  = Point(0,300)
-        self.space  = 20
-        self.step   = 75
+  def __init__(self):
+    self.height = 792
+    self.width  = 612
+    self.title  = Point(0,40)
+    self.image  = Point(0,300)
+    self.space  = 20
+    self.step   = 75
 
 config = Config()
-print(config.height)
 
+def make(year,month, img):
+  pdf_canvas = set_info("{0:04d}-{1:02d}".format(year,month))
+  print_title(pdf_canvas, year, month)
+  print_image(pdf_canvas, img)
+  print_box(pdf_canvas)    
+  print_word(pdf_canvas, year, month)
+  pdf_canvas.save()
 
-def make(year,month):
-    pdf_canvas = set_info("{0:04d}-{1:02d}".format(year,month))
-    print_title(pdf_canvas,'title')
-    print_image(pdf_canvas)
-    print_box(pdf_canvas)    
-    print_word(pdf_canvas)
-    pdf_canvas.save()
+def get_calendar(year, month):
+  d = i = date(year, month, 1)
+  pos = d.weekday()+1
+  calendar = [[]]
+  if pos != 7:
+    i -= timedelta(days=pos)
+    for w in range(0,pos):
+      calendar[0].append(i)
+      i += timedelta(days=1)
+      
+  while d.month >= i.month:
+    last = len(calendar) - 1
+    if len(calendar[last]) == 6:
+      calendar[last].append(i)
+      calendar.append([])
+    else:
+      calendar[last].append(i)
+    i += timedelta(days=1)
 
+  last = len(calendar) - 1
+  if len(calendar[last]) <= 6:
+    while len(calendar[last]) != 7:
+      calendar[last].append(i)
+      i += timedelta(days=1)
+  # for w in calendar:
+  #   for d in w:
+  #     print(str(d), end=" | ")
+  #   print()
+  return calendar
 
-# 初期設定
-def set_info(filename):
-    # print(letter) # height, width
-    pdf_canvas = canvas.Canvas("./{0}.pdf".format(filename), bottomup=False, pagesize=letter)  # 原点は左上
-        
-    pdf_canvas.setAuthor("4geru")
-    return pdf_canvas
+def print_title(pdf_canvas, year, month):
+  pdfmetrics.registerFont(UnicodeCIDFont("HeiseiKakuGo-W5"))
+  pdfmetrics.registerFont(UnicodeCIDFont("HeiseiMin-W3"))
+  pdf_canvas.setFont("HeiseiKakuGo-W5", 20)
+  pdf_canvas.drawString(config.space, config.title.height, "{0}年 {1}月のカレンダー".format(year, month))
 
-def print_title(pdf_canvas,word):
-    pdfmetrics.registerFont(UnicodeCIDFont("HeiseiKakuGo-W5"))
-    pdfmetrics.registerFont(UnicodeCIDFont("HeiseiMin-W3"))
-    pdf_canvas.setFont("HeiseiKakuGo-W5", 20)
-    pdf_canvas.drawString(config.space, config.title.height, "絵日記 2017/10/06(金) 名前 : さきさか しげる")
+def diff_month_color(weekday):
+  if weekday == 0:
+    #ffcdd2 red lighten-4
+    return color.HexColor("0xffcdd2")
+  elif weekday == 6:
+    #bbdefb blue lighten-4
+    return color.HexColor("0xbbdefb")
+  else :
+    #cfd8dc blue-grey lighten-4
+    return color.HexColor("0xcfd8dc")
 
-def print_word(pdf_canvas):
-    # フォントを登録する
-    pdfmetrics.registerFont(UnicodeCIDFont("HeiseiKakuGo-W5"))
+def same_month_color(weekday):
+  if weekday == 0:
+    #ef5350 red lighten-1
+    return color.HexColor("0xef5350")
+  elif weekday == 6:
+    #42a5f5 blue lighten-1
+    return color.HexColor("0x42a5f5")
+  else :
+    return color.black
 
-    # ゴシック体をサイズ15で
-    pdf_canvas.setFont("HeiseiKakuGo-W5", 20)
-    step = 40
-    idx = 0
-    arr = ['1','2','3','4','5','6','7']
-    # for i in range(0,8):
+def print_word(pdf_canvas, year, month):
+  # フォントを登録する
+  pdfmetrics.registerFont(UnicodeCIDFont("HeiseiKakuGo-W5"))
 
+  # ゴシック体をサイズ15で
+  pdf_canvas.setFont("HeiseiKakuGo-W5", 20)
+  calendar = get_calendar(year, month)
+
+  step = config.step
+  width = step * 7
+  top  = config.title.height + config.image.height + config.space * 3
+  left = (config.width - width)/2 + 2
+  idx = 0
+  for i in range(0,5):
+    for j in range(0,7):
+      if month == calendar[i][j].month:
+        pdf_canvas.setFillColor(same_month_color(j))
+      else:
+        pdf_canvas.setFillColor(diff_month_color(j))        
+      if calendar[i][j] == None:
+        break
+      pdf_canvas.drawString(j*step + left, i*step + top, str(calendar[i][j].day))
+      idx += 1
 
 def print_box(pdf_canvas):
   # 普通の線
@@ -76,16 +130,24 @@ def print_box(pdf_canvas):
     pdf_canvas.line(left, y, config.width - left, y)
 
 # 画像
-def print_image(pdf_canvas):
-    image = Image.open('./img/panda.jpeg')
-    image = image.transpose(Image.FLIP_TOP_BOTTOM)
-    # x,y,width,height
-    print(config.width - 400)
-    width = config.width - config.space * 2
-    height = config.image.height
-    top = config.title.height + config.space
-    print(letter)
-    pdf_canvas.drawInlineImage(image,config.width/2-width/2,top-height, width, height)
+def print_image(pdf_canvas, img):
+  image = Image.open(img)
+  image = image.transpose(Image.FLIP_TOP_BOTTOM)
+  # x,y,width,height
+  width = config.width - config.space * 2
+  height = config.image.height
+  top = config.title.height + config.space
+  pdf_canvas.drawInlineImage(image,config.width/2-width/2,top-height, width, height)
+
+
+# 初期設定
+def set_info(filename):
+  # print(letter) # height, width
+  pdf_canvas = canvas.Canvas("./calendar/{0}.pdf".format(filename), bottomup=False, pagesize=letter)  # 原点は左上
+      
+  pdf_canvas.setAuthor("4geru")
+  return pdf_canvas
 
 if __name__ == '__main__':
-    make(2018,1)
+  argv = sys.argv[1:]
+  make(int(argv[0]), int(argv[1]), argv[2])
