3d69b9228153983afaf408383eb581241a0cf5e6
Rp7rf <westhouse51@gmail.com>
2017-12-12 14:15:34 +0900

    ついに明日だー！！

--- make_pdf.py
+++ make_pdf.py
@@ -5,9 +5,11 @@ from reportlab.pdfbase import pdfmetrics
 from reportlab.pdfbase.cidfonts import UnicodeCIDFont
 from reportlab.lib.pagesizes import letter, landscape
 from datetime import date, timedelta
-import reportlab.lib.colors as color
+from urllib.request import urlopen
 from PIL import Image
+import reportlab.lib.colors as color
 import sys
+import io
 
 class Point:
   def __init__(self, width, height):
@@ -43,7 +45,7 @@ def get_calendar(year, month):
       calendar[0].append(i)
       i += timedelta(days=1)
       
-  while d.month >= i.month:
+  while d.month == i.month:
     last = len(calendar) - 1
     if len(calendar[last]) == 6:
       calendar[last].append(i)
@@ -64,36 +66,30 @@ def get_calendar(year, month):
   return calendar
 
 def print_title(pdf_canvas, year, month):
-  pdfmetrics.registerFont(UnicodeCIDFont("HeiseiKakuGo-W5"))
-  pdfmetrics.registerFont(UnicodeCIDFont("HeiseiMin-W3"))
-  pdf_canvas.setFont("HeiseiKakuGo-W5", 20)
+  font = 'HeiseiKakuGo-W5'
+  pdfmetrics.registerFont(UnicodeCIDFont(font))
+  pdf_canvas.setFont(font, 20)
   pdf_canvas.drawString(config.space, config.title.height, "{0}年 {1}月のカレンダー".format(year, month))
 
 def diff_month_color(weekday):
-  if weekday == 0:
-    #ffcdd2 red lighten-4
-    return color.HexColor("0xffcdd2")
-  elif weekday == 6:
-    #bbdefb blue lighten-4
-    return color.HexColor("0xbbdefb")
-  else :
-    #cfd8dc blue-grey lighten-4
-    return color.HexColor("0xcfd8dc")
+  #ffcdd2 red lighten-4
+  if weekday == 0: return color.HexColor("0xffcdd2")
+  #bbdefb blue lighten-4    
+  if weekday == 6: return color.HexColor("0xbbdefb")
+  #cfd8dc blue-grey lighten-4
+  return color.HexColor("0xcfd8dc")   
 
 def same_month_color(weekday):
-  if weekday == 0:
-    #ef5350 red lighten-1
-    return color.HexColor("0xef5350")
-  elif weekday == 6:
-    #42a5f5 blue lighten-1
-    return color.HexColor("0x42a5f5")
-  else :
-    return color.black
+  #ef5350 red lighten-1
+  if weekday == 0: return color.HexColor("0xef5350")
+  #42a5f5 blue lighten-1 
+  if weekday == 6: return color.HexColor("0x42a5f5")
+  return color.black
 
+# カレンダーの日付を表示
 def print_word(pdf_canvas, year, month):
-  # フォントを登録する
+  # # フォントを登録する
   pdfmetrics.registerFont(UnicodeCIDFont("HeiseiKakuGo-W5"))
-
   # ゴシック体をサイズ15で
   pdf_canvas.setFont("HeiseiKakuGo-W5", 20)
   calendar = get_calendar(year, month)
@@ -102,20 +98,16 @@ def print_word(pdf_canvas, year, month):
   width = step * 7
   top  = config.title.height + config.image.height + config.space * 3
   left = (config.width - width)/2 + 2
-  idx = 0
   for i in range(0,5):
     for j in range(0,7):
       if month == calendar[i][j].month:
         pdf_canvas.setFillColor(same_month_color(j))
       else:
-        pdf_canvas.setFillColor(diff_month_color(j))        
-      if calendar[i][j] == None:
-        break
+        pdf_canvas.setFillColor(diff_month_color(j)) 
       pdf_canvas.drawString(j*step + left, i*step + top, str(calendar[i][j].day))
-      idx += 1
 
+# カレンダーの線の表示
 def print_box(pdf_canvas):
-  # 普通の線
   pdf_canvas.setStrokeColor(color.black)
   step = config.step
   top  = config.title.height + config.image.height + config.space * 2
@@ -131,14 +123,18 @@ def print_box(pdf_canvas):
 
 # 画像
 def print_image(pdf_canvas, img):
-  image = Image.open(img)
-  image = image.transpose(Image.FLIP_TOP_BOTTOM)
+  image = ''
+  if img.find('http') == -1:
+    image = Image.open(img).transpose(Image.FLIP_TOP_BOTTOM)
+  else:
+    f = io.BytesIO(urlopen(img).read())
+    image = Image.open(f).transpose(Image.FLIP_TOP_BOTTOM)
+
   # x,y,width,height
-  width = config.width - config.space * 2
+  width = image.width * config.image.height / image.height
   height = config.image.height
   top = config.title.height + config.space
   pdf_canvas.drawInlineImage(image,config.width/2-width/2,top-height, width, height)
-
 
 # 初期設定
 def set_info(filename):
